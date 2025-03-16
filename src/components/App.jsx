6a882e28e2d5cb0ee20f0fb0c26e33f8189ab82c
Yuliia <lupin773311asd@gmail.com>
2025-03-16 21:06:11 +0000

    авп

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import Profile from "./Profile/Profile";  
-import userData from "../userData.json";   
+import Profile from "./Profile/Profile";
+import FriendList from "./FriendList/FriendList";
+import userData from "../userData.json";
+import friends from "../friends.json";
 
 const App = () => {
   return (
@@ -12,6 +14,7 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
     </div>
   );
 };
